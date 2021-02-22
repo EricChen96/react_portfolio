83fbcfe13adae3cc589b459d112c8fa6441a9e50
Eric Chen <ericchen201896@gmail.com>
2021-02-22 13:35:30 -0800

    created componenets for navtabs and sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
 import React, { useReducer, useRef } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <main class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <SideBar />
+            </div>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/SideBar.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+function SideBar() {
+    return (
+        <div>
+            <button className="btn btn-primary col-12 mx-auto mt-2" type="button" data-bs-toggle="collapse"
+                data-bs-target="#mainNavigationSidebar" aria-expanded="false" aria-controls="mainNavigationSidebar">
+                Main Profile
+                </button>
+            <div className="collapse" id="mainNavigationSidebar">
+                <div className="card card-body">
+                    {/* <a className="btn btn-primary col-12 mx-auto mt-2" href="../index.html">
+                        About Me
+                    </a> */}
+                    <Link to="/about" className="btn btn-primary col-12 mx-auto mt-2">About Me</Link>
+                    {/* <a className="btn btn-primary col-12 mx-auto mt-2" href="./contact.html">
+                        Contact
+                    </a> */}
+                    <Link to="/contact" className="btn btn-primary col-12 mx-auto mt-2">Contact</Link>
+                </div>
+            </div>
+            <button className="btn btn-primary col-12 mx-auto mt-2 projectPortfolioButton" type="button"
+                data-bs-toggle="collapse" data-bs-target="#portfolioSidebar" aria-expanded="false"
+                aria-controls="portfolioSidebar">
+                Project Portfolio
+            </button>
+            <div className="collapse" id="portfolioSidebar">
+                <div className="card card-body">
+                    {/* <a className="btn btn-primary col-12 mx-auto mt-2" href="./searchProjectScreen.html">
+                        Search Bar
+                    </a> */}
+                    <Link to="/search" className="btn btn-primary col-12 mx-auto mt-2">Search Bar</Link>
+                    <div className="div">
+                        Most Recent Searches:
+                    </div>
+
+                    <div className="recentSearchesButtonsList">
+                        {/* <!-- <a className="btn btn-primary col-12 mx-auto mt-2"
+                            href="https://anzelcapparelli.github.io/marvelHeroFinder/">
+                            Marvel Hero Finder
+                            </a> --> */}
+                    </div>
+                </div>
+            </div>
+            <a className="btn btn-primary col-12 mx-auto mt-2" href="./resume.pdf" download>Resume</a>
+        </div>
+
+    )
+}
+
+export default SideBar;
