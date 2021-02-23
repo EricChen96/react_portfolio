31427418de5cd1b4e818888252a73f6ec41d25c0
Eric Chen <ericchen201896@gmail.com>
2021-02-22 23:21:47 -0800

    added in projects component

---
+++ src/pages/Projects.js
@@ -0,0 +1,52 @@
+const projectList = [
+    {
+        title: "Marvel Hero Finder",
+        description: "Search up your favorite Marvel character!",
+        link: "https://anzelcapparelli.github.io/marvelHeroFinder/",
+        github: "https://github.com/anzelcapparelli/marvelHeroFinder",
+        image: "./Assets/Marvel_Screenshot.jpg"
+    },
+    {
+        title: "Weather Dashboard",
+        description: "Find out the weather of a city!",
+        link: "https://ericchen96.github.io/Weather_Dashboard/",
+        github: "https://github.com/EricChen96/Weather_Dashboard",
+        image: "./Assets/Weather_Dashboard_Screenshot.png"
+    },
+    {
+        title: "Code Quiz",
+        description: "Play a quick code quiz game to refresh your Javascript knowledge!",
+        link: "https://ericchen96.github.io/Code_Quiz/",
+        github: "https://github.com/EricChen96/Code_Quiz",
+        image: "./Assets/Code_Quiz_Screenshot.png"
+    },
+    {
+        title: "Employee Directory",
+        description: "Keep track of employees",
+        link: "",
+        github: "",
+        image: ""
+    },
+    {
+        title: "Online-Offline Budget Tracker",
+        description: "Keep track of your budgets online or offline",
+        link: "",
+        github: "",
+        image: "",
+    },
+    {
+        title: "Mean Game",
+        description: "Take over the world in a mean quote typing game",
+        link: "",
+        github: "",
+        image: "",
+    }
+];
+
+function Projects() {
+    return (
+        <div> 
+            
+        </div>
+    )
+}
